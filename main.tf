fb0448fdcce59b9415145fc58b50dfc2dba66e9e
shruthigit14 <120302169+shruthigit14@users.noreply.github.com>
2025-04-23 17:25:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-091dccf4e2d272bae"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "devopsbyraham"
+vpc_security_group_ids = ["sg-06087f584465ad8c2"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
